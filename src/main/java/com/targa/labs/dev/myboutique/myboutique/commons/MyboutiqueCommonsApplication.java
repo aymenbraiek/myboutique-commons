9d3f1750c8091750235037c88b16b5fc71c3a6c2
aymenbraiek <aymen.braiek@esprit.tn>
2021-05-11 19:04:57 +0100

    Intial Project

---
+++ src/main/java/com/targa/labs/dev/myboutique/myboutique/commons/MyboutiqueCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.targa.labs.dev.myboutique.myboutique.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboutiqueCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboutiqueCommonsApplication.class, args);
+	}
+
+}
